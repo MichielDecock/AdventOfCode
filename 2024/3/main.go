382d796523a32728f4260fde4423dc157c96aac0
Michiel Decock <michiel.decock@hexagon.com>
2024-12-03 11:16:11 +0100

    day 3 part 2

--- 2024/3/main.go
+++ 2024/3/main.go
@@ -11,21 +11,35 @@ import (
 func multiply(input string) int {
 	regex := regexp.MustCompile(`\d{1,3}`)
 	numbers := regex.FindAllString(input, -1);
+	if len(numbers) != 2 {
+		return 0
+	}
+
 	return utils.ToNumber(numbers[0]) * utils.ToNumber(numbers[1])
 }
 
 func main() {
 	lines := utils.ReadFile("input")
 
-	regex := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)`)
+	regex := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)|don't|do`)
 
 	sum := 0
+
+	enabled := true
 
 	for _, line := range lines {
 		hits := regex.FindAllString(line, -1);
 
 		for _, hit := range hits {
-			sum += multiply(hit)
+			if hit == "don't" {
+				enabled = false
+			} else if hit == "do" {
+				enabled = true
+			}
+
+			if enabled {
+				sum += multiply(hit)
+			}
 		}
 	}
 
