31b91d6d9eebf19ebc23508e38724faf93ec7152
Michiel Decock <michiel.decock@hexagon.com>
2024-12-03 10:47:42 +0100

    day 2 part 2

--- 2024/2/main.go
+++ 2024/2/main.go
@@ -8,50 +8,65 @@ import (
 	"aoc.com/utils"
 )
 
-func main() {
+func check(subfield[]string) bool {
 	const minDiff = 1
 	const maxDiff = 3
 
-	lines := utils.ReadFile("input")
+	var increasing bool
 
-	validReports := 0
+	for i := 0; i != len(subfield) - 1; i++ {
+		number1 := utils.ToNumber(subfield[i])
+		number2 := utils.ToNumber(subfield[i + 1])
 
-	for _, line := range lines {
-		fields := strings.Fields(line)
+		diff := number1 - number2
 
-		var increasing bool
-		valid := true
-
-		for i := 0; i != len(fields) - 1; i++ {
-			number1 := utils.ToNumber(fields[i])
-			number2 := utils.ToNumber(fields[i + 1])
-
-			diff := number1 - number2
-
-			if (i == 0) {
-				increasing = (diff >= 0)
-			}
-
-			if (increasing) {
-				if !(diff >= minDiff && diff <= maxDiff) {
-					valid = false
-				}
-			} else {
-				if !(diff <= -minDiff && diff >= -maxDiff) {
-					valid = false
-				}
-			}
+		if (i == 0) {
+			increasing = (diff >= 0)
 		}
 
-		if valid {
-			validReports++
+		if (increasing) {
+			if !(diff >= minDiff && diff <= maxDiff) {
+				return false
+			}
+		} else {
+			if !(diff <= -minDiff && diff >= -maxDiff) {
+				return false
+			}
 		}
 	}
 
-	// sum := 0
-	// for _, number := range list1 {
-	// 	sum += number * unique2[number]
-	// }
+	return true
+}
+
+func getSubfields(fields []string)  map[int][]string {
+	subfields := make(map[int][]string)
+	for i := 0; i != len(fields); i++ {
+		var subfield []string
+		for j := 0; j != len(fields); j++ {
+			if j != i {
+				subfield = append(subfield, fields[j])
+			}
+		}
+		subfields[i] = subfield
+	}
+
+	return subfields
+}
+
+func main() {
+	lines := utils.ReadFile("input")
+	validReports := 0
+
+	for _, line := range lines {
+		subfields := getSubfields(strings.Fields(line))
+
+		for i := 0; i != len(subfields); i++ {
+			if check(subfields[i]) {
+				validReports++
+				break
+			}
+		}
+	}
 
 	fmt.Println(strconv.FormatFloat(float64(validReports), 'f', -1, 64))
 }
