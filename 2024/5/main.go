89b3c1bb72266edfd2b2bbf88f7c1d1c6eaef41d
Michiel Decock <michiel.decock@hexagon.com>
2024-12-05 09:45:19 +0100

    day 5 part 2

--- 2024/5/main.go
+++ 2024/5/main.go
@@ -18,6 +18,19 @@ func found(pattern string, order []string) bool {
 	return false
 }
 
+func move(slice []string, from, to int) []string {
+    if from < 0 || from >= len(slice) || to < 0 || to >= len(slice) {
+        fmt.Println("Invalid index")
+        return slice
+    }
+    
+    item := slice[from]
+    slice = append(slice[:from], slice[from+1:]...)
+    slice = append(slice[:to], append([]string{item}, slice[to:]...)...)
+
+    return slice
+}
+
 func main() {
 	order := utils.ReadFile("order")
 	updates := utils.ReadFile("updates")
@@ -29,17 +42,30 @@ func main() {
 		hits := regex.FindAllString(update, -1)
 
 		valid := true
+		repeat := true
 
-		for i := 1; i < len(hits) && valid; i++ {
-			for j := 0; j < i && valid; j++ {
-				pattern := hits[j] + "|" + hits[i]
-				if !found(pattern, order) {
-					valid = false
+		for repeat {
+			repeat = false
+			stop := false
+			for i := 1; i < len(hits) && !stop; i++ {
+				for j := 0; j < i && !stop; j++ {
+					pattern := hits[j] + "|" + hits[i]
+					if !found(pattern, order) {
+						stop = true
+						valid = false
+						repeat = true
+						newPattern := hits[i] + "|" + hits[j]
+						if !found(newPattern, order) {
+							fmt.Println("new pattern should be found!")
+						}
+
+						move(hits, i, j)
+					}
 				}
 			}
 		}
 
-		if valid {
+		if !valid {
 			middle := len(hits) / 2
 			total += utils.ToNumber(hits[middle])
 		}
