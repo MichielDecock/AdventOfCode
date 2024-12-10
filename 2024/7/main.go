0f5b6cee621cd9d91da650fabbebdfe0343c37b6
Michiel Decock <michiel.decock@hexagon.com>
2024-12-10 09:21:23 +0100

    day 7 part 2

--- 2024/7/main.go
+++ 2024/7/main.go
@@ -9,13 +9,27 @@ import (
 	"aoc.com/utils"
 )
 
+func ternary(number int, out *int, order int) {
+	if number == 0 {
+		return
+	}
+
+	order++
+
+	ternary(number/3, out, order)
+
+	*out |= ((number % 3) << (2 * order))
+}
+
 func calc(numbers int, input []string) int {
 	local := utils.ToNumber(input[0])
 	for i := 1; i < numbers; i++ {
 		if input[2*i-1] == "+" {
 			local += utils.ToNumber(input[2*i])
-		} else {
+		} else if input[2*i-1] == "*" {
 			local *= utils.ToNumber(input[2*i])
+		} else {
+			local = utils.ToNumber(strconv.Itoa(local) + input[2*i])
 		}
 	}
 	return local
@@ -23,13 +37,21 @@ func calc(numbers int, input []string) int {
 
 func valid(result int, input []string) bool {
 	operators := len(input) / 2
-	limit := math.Pow(2, float64(operators))
+	limit := math.Pow(3, float64(operators))
 	for i := 0; i != int(limit); i++ {
+		number := 0
+		order := -1
+		ternary(i, &number, order)
 		for j := 0; j != operators; j++ {
-			if i&int(math.Pow(float64(2), float64(j))) != 0 {
-				input[2*j+1] = "*"
-			} else {
+			bits := number & (0b11 << (2 * j))
+			bits >>= (2 * j)
+
+			if bits == 0 {
 				input[2*j+1] = "+"
+			} else if bits == 1 {
+				input[2*j+1] = "||"
+			} else {
+				input[2*j+1] = "*"
 			}
 			if result == calc(operators+1, input) {
 				return true
