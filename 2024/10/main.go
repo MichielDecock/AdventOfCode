2d40ebe02936656e8a9367d85b9b67be4544d9a1
Michiel Decock <michiel.decock@hexagon.com>
2024-12-10 14:00:44 +0100

    day 10 part 1

---
+++ 2024/10/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+
+	"aoc.com/utils"
+)
+
+type Pos struct {
+	Row int
+	Col int
+}
+
+func findPath(number int, pos Pos, plan [][]int, found *[][]bool) int {
+	if plan[pos.Row][pos.Col] != number {
+		return 0
+	}
+
+	if plan[pos.Row][pos.Col] == 9 {
+		if (*found)[pos.Row][pos.Col] {
+			return 0
+		}
+		(*found)[pos.Row][pos.Col] = true
+		return 1
+	}
+
+	paths := 0
+
+	for row := int(math.Max(0, float64(pos.Row-1))); row != int(math.Min(float64(pos.Row+2), float64(len(plan)))); row++ {
+		for col := int(math.Max(0, float64(pos.Col-1))); col != int(math.Min(float64(pos.Col+2), float64(len(plan[0])))); col++ {
+			if row == pos.Row && col == pos.Col {
+				continue
+			}
+			if math.Abs(float64(row-pos.Row)) == 1 && math.Abs(float64(col-pos.Col)) == 1 {
+				continue
+			}
+			paths += findPath(number+1, Pos{Row: row, Col: col}, plan, found)
+		}
+	}
+
+	return paths
+}
+
+func convert(plan []string) [][]int {
+	var matrix [][]int
+	for _, str := range plan {
+		var row []int
+
+		for _, elem := range str {
+			if elem == '.' {
+				row = append(row, -1)
+			} else {
+				row = append(row, utils.ToNumber(string(elem)))
+			}
+		}
+
+		matrix = append(matrix, row)
+	}
+	return matrix
+}
+
+func initFound(plan [][]int) [][]bool {
+	found := make([][]bool, len(plan))
+	for i := range found {
+		found[i] = make([]bool, len(plan[0]))
+	}
+	return found
+}
+
+func main() {
+	input := convert(utils.ReadFile("input"))
+
+	total := 0
+
+	for row, line := range input {
+		for col := range line {
+			found := initFound(input)
+			res := findPath(0, Pos{Row: row, Col: col}, input, &found)
+			total += res
+		}
+	}
+
+	fmt.Println(strconv.FormatFloat(float64(total), 'f', -1, 64))
+
+}
