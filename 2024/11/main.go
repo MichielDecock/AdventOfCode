83cf504cc7d3ef06bd7f7561fc522fea643c3ed8
Michiel Decock <michiel.decock@hexagon.com>
2024-12-12 10:17:59 +0100

    day 11 part 2

--- 2024/11/main.go
+++ 2024/11/main.go
@@ -8,26 +8,30 @@ import (
 	"aoc.com/utils"
 )
 
-type Pos struct {
-	Row int
-	Col int
-}
-
-func convert(input string) []int {
-	var out []int
+func convert(input string) map[int]int {
+	out := make(map[int]int)
 	fields := strings.Fields(input)
 	for _, field := range fields {
-		out = append(out, utils.ToNumber(field))
+		add(&out, utils.ToNumber(field), 1)
 	}
 	return out
 }
 
-func blink(stones []int) []int {
-	var out []int
+func add(out *map[int]int, stone int, count int) {
+	_, ok := (*out)[stone]
+	if !ok {
+		(*out)[stone] = count
+	} else {
+		(*out)[stone] += count
+	}
+}
 
-	for _, stone := range stones {
+func blink(stones map[int]int) map[int]int {
+	out := make(map[int]int)
+
+	for stone, count := range stones {
 		if stone == 0 {
-			out = append(out, 1)
+			add(&out, 1, count)
 			continue
 		}
 
@@ -36,25 +40,33 @@ func blink(stones []int) []int {
 
 		if len%2 == 0 {
 			half := len / 2
-			out = append(out, utils.ToNumber(str[:half]))
-			out = append(out, utils.ToNumber(str[half:]))
+			add(&out, utils.ToNumber(str[:half]), count)
+			add(&out, utils.ToNumber(str[half:]), count)
 			continue
 		}
 
-		out = append(out, stone*2024)
+		add(&out, 2024*stone, count)
 	}
 
 	return out
 }
 
+func total(stones map[int]int) int {
+	total := 0
+	for _, count := range stones {
+		total += count
+	}
+	return total
+}
+
 func main() {
 	stones := convert(utils.ReadFile("input")[0])
 
-	for count := 0; count != 25; count++ {
+	for count := 0; count != 75; count++ {
 		stones = blink(stones)
 	}
 
-	total := len(stones)
+	total := total(stones)
 
 	fmt.Println(strconv.FormatFloat(float64(total), 'f', -1, 64))
 }
