5e4208d7732d287ec5fec40c774440c5bb9e24ef
Michiel Decock <michiel.decock@hexagon.com>
2025-12-01 10:40:00 +0100

    Day 1 part 2

--- 2025/main.py
+++ 2025/main.py
@@ -1,6 +1,7 @@
 #! /opt/homebrew/bin/python3
 
 import re
+import math
 
 if __name__ == "__main__":
     hits = 0
@@ -9,11 +10,32 @@ if __name__ == "__main__":
         for line in file.readlines():
             distance = int(re.findall(r"\d+", line)[0])
             direction = line[0]
-            if direction == "L":
-                pos = (pos - distance) % 100
-            elif direction == "R":
-                pos = (pos + distance) % 100
 
+            wasZero = pos == 0
+
+            if direction == "L":
+                pos -= distance
+            elif direction == "R":
+                pos += distance
+            
+            if pos == 0:
+                if not wasZero:
+                    hits += 1
+                continue
+
+            if pos > 99:
+                while pos > 99:
+                    pos -= 100
+                    hits += 1
+                continue
+
+            if pos < 0:
+                while pos < 0:
+                    pos += 100
+                    hits += 1
+                if wasZero:
+                    hits -= 1
+            
             if pos == 0:
                 hits += 1
 
