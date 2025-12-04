446af5e00f5194e29d02dd88a65c9a3562054fd6
Michiel Decock <michiel.decock@hexagon.com>
2025-12-04 10:39:21 +0100

    Day 4 part 2

--- 2025/4/main.py
+++ 2025/4/main.py
@@ -7,21 +7,29 @@ if __name__ == "__main__":
     with open("input") as file:
         matrix = [list(line.rstrip("\n")) for line in file]
         
-        for i, el in enumerate(matrix):
-            for j, el2 in enumerate(el):
-                if el2 != '@':
-                    continue
+        process = True
+        while process:
+            process = False
+            matrixCopy = copy.deepcopy(matrix)
+            for i, el in enumerate(matrix):
+                for j, el2 in enumerate(el):
+                    if el2 != '@':
+                        continue
 
-                hits = 0
-                for k in range(max(i - 1, 0), min(i + 1, len(matrix) - 1) + 1):
-                    for l in range(max(j - 1, 0), min(j + 1, len(el) - 1) + 1):
-                        if k == i and l == j:
-                            continue
+                    hits = 0
+                    for k in range(max(i - 1, 0), min(i + 1, len(matrix) - 1) + 1):
+                        for l in range(max(j - 1, 0), min(j + 1, len(el) - 1) + 1):
+                            if k == i and l == j:
+                                continue
 
-                        if matrix[k][l] == '@':
-                            hits += 1
-                
-                if hits < 4:
-                    sum += 1
+                            if matrix[k][l] == '@':
+                                hits += 1
+                    
+                    if hits < 4:
+                        process = True
+                        matrixCopy[i][j] = 'x'
+                        sum += 1
+            
+            matrix = copy.deepcopy(matrixCopy)
 
     print(sum)
