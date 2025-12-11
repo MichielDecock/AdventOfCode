37d02997f8c385ce36877840c56ee73d49bf69c4
Michiel Decock <michiel.decock@hexagon.com>
2025-12-11 16:43:10 +0100

    Day 11 part 2

--- 2025/11/main.py
+++ 2025/11/main.py
@@ -13,26 +13,41 @@ def parse(file):
             m[v[0]] = v[1:]
         return m
     
-def iterate(m):
-    posIn = 'you'
-    posOut = 'out'
-
-    queue = [posIn]
-    paths = 0
+def iterate(m, posIn, posOut):
+    queue = [[posIn, 1]]
+    numPaths = 0
 
     while len(queue) > 0:
-        p = queue.pop(0)
+        e = queue.pop(0)
+        p = e[0]
+        count = e[1]
 
         if p == posOut:
-            paths += 1
+            numPaths += count
+            continue
+
+        if p == 'out':
             continue
 
         for el in m[p]:
-            queue.append(el)
+            found = False
+            for index, s in enumerate(queue):
+                if s[0] == el:
+                    queue[index][1] += count
+                    found = True
+                    break
 
-    return paths
+            if not found:
+                queue.append([el, count])
+
+    return numPaths
 
 if __name__ == '__main__':
     m = parse('input')
 
-    print(iterate(m))
+    fft2dac = iterate(m, 'fft', 'dac')
+    fft2out = iterate(m, 'fft', 'out')
+    dac2out = iterate(m, 'dac', 'out')
+    dac2fft = iterate(m, 'dac', 'fft')
+
+    print(iterate(m, 'svr', 'fft') * fft2dac * dac2out + iterate(m, 'svr', 'dac') * dac2fft * fft2out)
