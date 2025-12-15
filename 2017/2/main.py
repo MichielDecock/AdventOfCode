407531225714e157c89cbd139614365b49ff1344
Michiel Decock <michiel.decock@hexagon.com>
2025-12-15 19:07:13 +0100

    Day 2 part 2

--- 2017/2/main.py
+++ 2017/2/main.py
@@ -2,8 +2,18 @@ def parse(file):
     with open(file) as f:
         return [sorted([int(x) for x in l.strip().split('\t')], reverse=True) for l in f.readlines()]
     
+def findMultiple(d):
+    for n1 in d:
+        for n2 in d:
+            if n1 == n2:
+                continue
+
+            if max(n2, n1) % min(n2, n1) == 0:
+                return max(n2, n1) // min(n2, n1)
+    return 0
+    
 def checkSum(data):
-    return sum([max(d) - min(d) for d in data])
+    return sum(findMultiple(d) for d in data)
 
 if __name__ == "__main__":
     data = parse('input')
--- 2017/2/test.py
+++ 2017/2/test.py
@@ -3,7 +3,7 @@ from main import checkSum
 
 class MyTestCase(unittest.TestCase):
     def test_check1(self):
-        self.assertEqual(checkSum([[5, 1, 9, 5], [7, 5, 3], [2, 4, 6, 8]]), 18)
+        self.assertEqual(checkSum([[5, 9, 2, 8], [9, 4, 7, 3], [3, 8, 6, 5]]), 9)
 
 
 if __name__ == "__main__":
