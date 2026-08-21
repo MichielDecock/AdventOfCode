7d76f2fbf7727359b9a9e9efc240f00e4064ff0d
Michiel Decock <michiel.decock@octave.com>
2026-08-21 16:06:29 +0200

    Day 6 part 2 (2020)

--- 2020/6.py
+++ 2020/6.py
@@ -5,13 +5,26 @@ def parse(filename):
     lines = []
     fullFilename = os.path.join(os.path.dirname(__file__), filename)
     with open(fullFilename) as file:
-        curLine = ''
+        curLine = set()
+        skip = False
         for line in [line.strip('\n') for line in file]:
             if not line:
-                lines.append(''.join(set(curLine)))
-                curLine = ''
-            else:
-                curLine += line
+                if curLine:
+                    lines.append(curLine)
+                curLine = set()
+                skip = False
+                continue
+
+            if skip:
+                continue
+
+            if not curLine:
+                curLine = set(line)
+                continue
+
+            curLine = curLine.intersection(line)
+            if not curLine:
+                skip = True
 
     return lines
 
