83ef3fdfb357883b993d36e1bbd4ab5cba5ae51d
Michiel Decock <michiel.decock@octave.com>
2026-08-21 15:10:02 +0200

    Day 5 part 2 (2020)

--- 2020/5.py
+++ 2020/5.py
@@ -8,7 +8,7 @@ class Seat:
     col: int
 
 
-def parse(filename: str):
+def parse(filename: str, selectRow: str = ''):
     table = str.maketrans({
         'F': '0',
         'B': '1',
@@ -19,19 +19,48 @@ def parse(filename: str):
     lines = []
     fullFilename = os.path.join(os.path.dirname(__file__), filename)
     with open(fullFilename) as file:
-        for line in [line.strip('\n') for line in file]:
+        for line in [line.strip('\n') for line in file if not selectRow or line[:7] == selectRow]:
             line = line.translate(table)
             lines.append(line)
 
     return lines
 
-def findSeat(line):
-    return Seat(int(line[:7], 2),int(line[-3:], 2))
+def row(line):
+    return int(line[:7], 2)
 
+def column(line):
+    return int(line[-3:], 2)
+
+def rows(lines):
+    rows = sorted([row(line) for line in lines])
+    return [el for el in rows if el != min(rows) and el != max(rows)]
+
+def columns(lines):
+    return sorted([column(line) for line in lines])
+
+
+def findRow(rows):
+    for row in rows:
+        if rows.count(row) != 8:
+            return row
+    return None
+
+def rowToString(row):
+    table = str.maketrans({
+            '0': 'F',
+            '1': 'B'
+        })
+
+    return str(format(row, 'b').translate(table))
 
 def seatID(seat):
     return seat.row * 8 + seat.col
 
 if __name__ == "__main__":
     lines = parse('input')
-    print(max([seatID(findSeat(line)) for line in lines]))
+    candidateRow = findRow(rows(lines))
+    candidates = columns(parse('input', rowToString(candidateRow)))
+    for col in range(8):
+        if col not in candidates:
+            assert(candidateRow)
+            print(seatID(Seat(candidateRow, col)))
