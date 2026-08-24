55db6fee4644d7cd5edbb4bdc2c512b343e4ae0f
Michiel Decock <michiel.decock@octave.com>
2026-08-24 10:00:05 +0200

    Day 7 part 2 (2020)

--- 2020/7.py
+++ 2020/7.py
@@ -1,9 +1,22 @@
+from __future__ import annotations
+
 import os
+from dataclasses import dataclass
 
 
 _LAST_ID = 0
 _ID_MAP = dict()
 _TARGET = 'shiny gold'
+
+@dataclass
+class Node:
+    id: int
+    children: list[tuple[Node, int]]
+
+    def __eq__(self, other):
+        if isinstance(other, int):
+            return self.id == other
+        return isinstance(other, Node) and self.id == other.id
 
 
 def parse(filename):
@@ -16,7 +29,6 @@ def parse(filename):
             line = line.replace('bag',' ')
             line = line.replace('no other',' ')
             line = line.replace('.',' ')
-            line = ''.join(c for c in line if not c.isdigit())
             [key, _values] = [l.strip() for l in line.split('contain')]
             values = [v.strip() for v in _values.split(',') if v.strip()]
             if key not in _ID_MAP.keys():
@@ -26,25 +38,36 @@ def parse(filename):
 
     return lines
 
-def tagDirectContainment(lines):
-    return [_ID_MAP[key] for key in lines if _TARGET in lines[key]]
+def makeNodes(keys):
+    return [Node(_ID_MAP[key], []) for key in keys]
 
-def containsTarget(key, lines, holders):
-    if _ID_MAP[key] in holders:
-        return True
 
-    if _TARGET in lines[key]:
-        holders.append(_ID_MAP[key])
-        return True
+def makeTree(lines):
+    nodes = makeNodes(lines.keys())
+    for key, values in lines.items():
+        children = [(nodes[nodes.index(_ID_MAP[i[1]])], int(i[0])) for i in (v.split(' ', 1) for v in values)]
+        nodes[nodes.index(_ID_MAP[key])].children = children
 
-    for v in lines[key]:
-        if containsTarget(v, lines, holders):
-            return True
+    return nodes
 
-    return False
+def totalBags(tree):
+    node = tree[tree.index(_ID_MAP[_TARGET])]
+    toVisit = [(node, 0)]
+
+    total = 0
+
+    while toVisit:
+        node, localTotal = toVisit[0]
+        total += localTotal
+        toVisit.pop(0)
+
+        for n, count in node.children:
+            toVisit.append((n, max(localTotal, 1) * count))
+
+    return total
 
 
 if __name__ == "__main__":
     lines = parse('input')
-    holders = tagDirectContainment(lines)
-    print(sum(1 for key in lines if containsTarget(key, lines, holders)))
+    tree = makeTree(lines)
+    print(totalBags(tree))
