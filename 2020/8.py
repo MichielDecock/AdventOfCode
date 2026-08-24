371dc002792bc2153ede15414e5aea59c1b22ff9
Michiel Decock <michiel.decock@octave.com>
2026-08-24 11:28:14 +0200

    Day 8 part 2 (2020)

--- 2020/8.py
+++ 2020/8.py
@@ -1,6 +1,7 @@
 import os
 from dataclasses import dataclass
 from enum import Enum
+import copy
 
 class Operation(Enum):
     ACC = 1,
@@ -31,17 +32,15 @@ def parse(filename):
 
     return lines
 
-
-if __name__ == "__main__":
-    lines = parse('input')
-
-    acc = 0
-    pos = 0
+def reachEnd(lines, pos, acc):
+    numLines = len(lines)
     while True:
+        if pos >= numLines:
+            return True, acc
+
         instruction = lines[pos]
         if instruction.processed:
-            print(acc)
-            break
+            return False
 
         instruction.processed = True
 
@@ -53,4 +52,40 @@ if __name__ == "__main__":
             acc += instruction.count
 
         pos += 1
+
+
+if __name__ == "__main__":
+    lines = parse('input')
+
+    acc = 0
+    pos = 0
+    while True:
+        instruction = lines[pos]
+
+        if instruction.operation == Operation.ACC:
+            instruction.processed = True
+            acc += instruction.count
+            pos += 1
+            continue
+
+        if out := reachEnd(copy.deepcopy(lines), pos, acc):
+            print(out)
+            break
+
+        copyLines = copy.deepcopy(lines)
+        if copyLines[pos].operation == Operation.JMP:
+            copyLines[pos].operation = Operation.NOP
+        elif copyLines[pos].operation == Operation.NOP:
+            copyLines[pos].operation = Operation.JMP
+
+        if out := reachEnd(copyLines, pos, acc):
+            print(out)
+            break
+
+        if lines[pos].operation == Operation.JMP:
+            pos += copyLines[pos].count
+        elif lines[pos].operation == Operation.NOP:
+            pos += 1
+
+        instruction.processed = True
             
