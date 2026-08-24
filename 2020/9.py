a8362e84676032889f147f56956304f662e833b4
Michiel Decock <michiel.decock@octave.com>
2026-08-24 21:04:01 +0200

    Day 9 part 2 (2020)

--- 2020/9.py
+++ 2020/9.py
@@ -19,14 +19,38 @@ def findParts(preamble, target):
 
     return False
 
-
-if __name__ == "__main__":
-    lines = parse('input')
-
+def findProblem(lines):
     for pos in range(_CUTOFF, len(lines)):
         item = lines[pos]
         preamble = lines[pos -_CUTOFF: pos]
 
         if not findParts(preamble, item):
-            print(item)
+            return (item, pos)
+
+    return (None, None)
+
+
+if __name__ == "__main__":
+    lines = parse('input')
+    target, index = findProblem(lines)
+    assert(target)
+    assert(index)
+
+    start = 0
+    end = 1
+    while True:
+        if end > len(lines):
             break
+
+        s = sum(lines[start:end])
+        while s < target:
+            s += lines[end]
+            end += 1
+
+        if s == target:
+            print(min(lines[start:end]) + max(lines[start:end]))
+            break
+
+        while s > target:
+            s -= lines[start]
+            start += 1
